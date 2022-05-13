6fe745b1aea7f4b867e9ed0404df7317370abadd
MARLIN K MAKORI <marlin.makori@gmail.com>
2022-05-13 12:43:32 -0700

    competitions

--- routes/schedules.js
+++ routes/schedules.js
@@ -6,7 +6,10 @@ const fbsCtrl = require('../controllers/fbs.js')
    
 router.get('/fcs', fcsCtrl.getFcs)
 router.get('/fcs/:confName', fcsCtrl.getConfSchools)
+router.get('/fcs/comp/:schoolName', fcsCtrl.getSchoolComp)
 router.get('/fbs', fbsCtrl.getFbs)
+router.get('/fbs/:confName', fbsCtrl.getConfSchools)
+router.get('/fbs/comp/:schoolName', fbsCtrl.getSchoolComp)
 // router.get('/fcs/:accountId', fcsCtrl.getFcsAccount())
 
 
