6d3a771035c07fd8191d3c83d6e52a5fee8a9093
MARLIN K MAKORI <marlin.makori@gmail.com>
2022-05-16 04:15:23 -0700

    test

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-  }) 
 
 app.use(function (req, res, next) {
     res.locals.user = req.user
