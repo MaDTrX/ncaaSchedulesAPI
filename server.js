1d5db16291f1afb4bcdc51049327abea7dca2706
MARLIN K MAKORI <marlin.makori@gmail.com>
2022-05-16 04:42:28 -0700

    header

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-  }) 
 
 app.use(function (req, res, next) {
     res.locals.user = req.user
@@ -23,6 +20,9 @@ app.use(function (req, res, next) {
 
 app.use('/', schedulesRouter)
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+  }) 
 
 app.listen(process.env.PORT || 5000, () => console.log('server listening'))
 
